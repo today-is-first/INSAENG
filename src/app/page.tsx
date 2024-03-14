c463ac3f56280d0a1bb3182ca92355f8641349c6
today-is-first <diligent0644@gmail.com>
2024-03-14 16:55:04 +0900

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import Header from '@/components/@common/header';
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+    </main>
+  );
+}
---
+++ src/components/@common/header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="border-b border-b-slate-950">
+      <section className="w-4/5 my-5 mx-auto">
+        <div className="text-3xl text-slate-950 font-black">:INTJ</div>
+      </section>
+    </header>
+  );
+}
